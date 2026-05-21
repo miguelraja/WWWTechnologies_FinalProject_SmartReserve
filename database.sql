202c277d72961a8bcef7e2328a2c5d10aafeb41c
miguelraja <migrajolm@alum.us.es>
2026-05-21 21:17:43 +0200

    changes for infinityfree deployment

--- database.sql
+++ database.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS rooms (
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS bookings (
 );
 
 INSERT INTO rooms(name, type, capacity) VALUES
-('Room 1', Large, 15),
-('Room 2', Large, 20),
-('Room 3', Small, 5),
-('Room 4', Small, 8);
+('Room 1', "Large", 15),
+('Room 2', "Large", 20),
+('Room 3', "Small", 5),
+('Room 4', "Small", 8);
--- db_connection.php
+++ db_connection.php
@@ -1,12 +1,11 @@
 <?php
-    $host = 'localhost';
+    $host = 'sql211.byetcluster.com';
     $dbName = 'smartreserve';
-    $username = 'root';
-    $password = '12345';
-    $port = '3307';
+    $username = 'if0_41987345';
+    $password = 'Lolazo2005';
 
     try {
-        $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbName;charset=utf8mb4", $username, $password);
+        $pdo = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8mb4", $username, $password);
 
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         //echo "Connected successfully to the database.";
